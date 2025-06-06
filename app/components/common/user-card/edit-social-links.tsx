e3d4b3af67382f15bdcb048977e60605fdb2a117
caiomendes <caiosamarone10@hotmail.com>
2025-06-06 18:44:19 -0300

    feat: social media feature

--- app/components/common/user-card/edit-social-links.tsx
+++ app/components/common/user-card/edit-social-links.tsx
@@ -8,17 +8,22 @@ import { useParams, useRouter } from 'next/navigation'
 
 import TextInput from '../../ui/text-input'
 import createSocialLinks from '@/app/actions/create-social-links'
+import { ProfileData } from '@/app/server/get-profile-data'
 
-export default function EditSocialLinks() {
+export default function EditSocialLinks({
+  socialMedias,
+}: {
+  socialMedias?: ProfileData['socialMedias']
+}) {
   const router = useRouter()
 
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [isSavingSocialLinks, setIsSavingSocialLinks] = useState(false)
 
-  const [github, setGithub] = useState('')
-  const [instagram, setInstagram] = useState('')
-  const [linkedin, setLinkedin] = useState('')
-  const [twitter, setTwitter] = useState('')
+  const [github, setGithub] = useState(socialMedias?.github ?? '')
+  const [instagram, setInstagram] = useState(socialMedias?.instagram ?? '')
+  const [linkedin, setLinkedin] = useState(socialMedias?.linkedin ?? '')
+  const [twitter, setTwitter] = useState(socialMedias?.twitter ?? '')
 
   const { profileId } = useParams()
 
--- app/components/common/user-card/user-card.tsx
+++ app/components/common/user-card/user-card.tsx
@@ -2,10 +2,16 @@ import { Github, Instagram, Linkedin, Twitter, Plus } from 'lucide-react'
 import Button from '../../ui/button'
 import SocialMediaButton from '../social-media-button'
 import EditSocialLinks from './edit-social-links'
+import Link from 'next/link'
+import { ProfileData } from '@/app/server/get-profile-data'
 
 const icons = [Github, Instagram, Linkedin, Twitter]
 
-export default function UserCard() {
+export default function UserCard({
+  profileData,
+}: {
+  profileData?: ProfileData
+}) {
   return (
     <div className='w-[348px] flex flex-col gap-5 items-center p-5  border-white border-opacity-10 bg-[#121212] rounded-3xl text-white'>
       <div className='size-48'>
@@ -26,12 +32,49 @@ export default function UserCard() {
       <div className='flex flex-col gap-2 w-full'>
         <span className='uppercase text-xs font-medium'>Links</span>
         <div className='flex gap-3 justify-center'>
-          {icons.map((Icon, index) => (
+          {profileData?.socialMedias.github && (
+            <Link
+              href={profileData?.socialMedias.github}
+              target='_blank'
+              className='p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]'
+            >
+              <Github />
+            </Link>
+          )}
+          {profileData?.socialMedias.instagram && (
+            <Link
+              href={profileData?.socialMedias.instagram}
+              target='_blank'
+              className='p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]'
+            >
+              <Instagram />
+            </Link>
+          )}
+          {profileData?.socialMedias.linkedin && (
+            <Link
+              href={profileData?.socialMedias.linkedin}
+              target='_blank'
+              className='p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]'
+            >
+              <Linkedin />
+            </Link>
+          )}
+          {profileData?.socialMedias.twitter && (
+            <Link
+              href={profileData?.socialMedias.twitter}
+              target='_blank'
+              className='p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]'
+            >
+              <Twitter />
+            </Link>
+          )}
+
+          {/* {icons.map((Icon, index) => (
             <SocialMediaButton key={index}>
               <Icon size={20} />
             </SocialMediaButton>
-          ))}
-          <EditSocialLinks />
+          ))} */}
+          <EditSocialLinks socialMedias={profileData?.socialMedias} />
         </div>
       </div>
       <div className='flex flex-col gap-3 w-full h-[172px]'>
