08fa7481086e98b85bf94802c898d88e445d3dac
caiomendes <caiosamarone10@hotmail.com>
2025-08-13 19:07:03 -0300

    chore: fix mixpanel

--- app/lib/mixpanel.ts
+++ app/lib/mixpanel.ts
@@ -1,6 +1,6 @@
 import Mixpanel from 'mixpanel'
 
-const mixpanelEvent = Mixpanel.init('abac56da6bebc29049a9d5ac82a4cab5')
+const mixpanelEvent = Mixpanel.init('e876bfbbca22f5d213a6579a279603b8')
 
 export function trackServerEvent(eventName: string, properties: any) {
   if (process.env.NODE_ENV === 'development') return
